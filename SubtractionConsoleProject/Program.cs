ca37a87dfbe269224358899f62988f623c18980d
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2020-07-16 20:40:49 -0400

    Added projects and references

---
+++ SubtractionConsoleProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SubtractionConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SubtractionProject/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SubtractionProject
+{
+    public class Class1
+    {
+    }
+}
